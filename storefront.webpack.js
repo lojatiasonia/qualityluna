50c8281dc6afefc9b82338c933ec36d7c47b3082
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2020-03-19 10:26:22 -0300

    Create storefront.webpack.js

---
+++ storefront.webpack.js
@@ -0,0 +1,8 @@
+module.exports = {
+  resolve: {
+    alias: {
+      './html/EcVariations.html$':
+        require('path').resolve(__dirname, 'template/js/components/EcVariations.html')
+    }
+  }
+}
