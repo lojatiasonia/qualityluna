e1016feaec818003baa86223c026fac17ad5a111
Matheus <matheus@e-com.club>
2020-03-24 17:58:25 -0300

    chore: update page

--- storefront.webpack.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  resolve: {
-    alias: {
-      './html/EcVariations.html$':
-        require('path').resolve(__dirname, 'template/js/components/EcVariations.html')
-    }
-  }
-}
